d8456ddec65300d99034161018469a441fedc5c8
blankaex <blankaex@protonmail.com>
2020-07-29 21:39:59 +1000

    v1.0

---
+++ readme.md
@@ -0,0 +1,3 @@
+# devola
+
+Based off [Devola by damienstewart](https://github.com/damienstewart/Startpages/tree/master/Devola%20Startpage).
---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $(document).keypress(function(event) {
+      if ( event.key == 's' && document.activeElement != document.getElementById("search-bar") ) {
+        document.getElementById("search-bar").focus();
+        document.getElementById("search-bar").value = "";
+      }
+    });
+
+
+    $("#search-bar").keypress(function(event) {
+      if ( event.key == 'Enter' ) {
+        var engines = {
+          '-g': 'https://google.com/search?q=',
+          '-i': 'https://www.google.com/search?tbm=isch&q=',
+          '-y': 'https://youtube.com/results?search_query=',
+          '-j': 'http://jisho.org/search/',
+          '-k': 'https://kotobank.jp/gs/?q=',
+          '-n': 'https://nyaa.si/?f=0&c=0_0&q=',
+          '-s': 'https://sukebei.nyaa.si/?f=0&c=0_0&q=',
+          '-p': 'https://www.anime-planet.com/anime/all?name=',
+          '-v': 'https://vndb.org/v/all?sq=',
+          '-a': 'https://wiki.archlinux.org/index.php?search=',
+          '-w': 'https://en.wikipedia.org/w/index.php?search='
+        };
+
+        var args   = $("#search-bar").val().split(' '),
+            prefix = args[0],
+            engine = engines['-g'];
+
+        if ( prefix in engines ) {
+          engine = engines[prefix]
+          args = args.slice(1)
+        };
+
+        window.location.href = engine + encodeURIComponent(args.join(' '));
+      }
+    });
+
+});	
